c046d7b471dac4084777f5ffcc1283f8b19eeae3
Goldenfaeron <tobias@mreich.de>
2020-06-29 13:59:46 +0200

    no message

--- mixins/windowSize.js
+++ mixins/windowSize.js
@@ -30,7 +30,7 @@ export const Mixin = {
           return {text: 'body-2', bigText: 'headline', subtitle: 'title', headline: 'display-1', lgtext: 'display-2', svgSize: '40%'};
         case "lg":
           return {text: ' headline', bigText: 'display-1', subtitle: ' display-2', headline: ' display-3', lgtext: ' display-4', svgSize: '30%'};
-        case "xl":u 
+        case "xl":
           return {text: 'display-1', bigText: 'display-2', subtitle: 'display-2', headline: 'display-3', lgtext: 'display-4', svgSize: '30%'};
       }
     }
